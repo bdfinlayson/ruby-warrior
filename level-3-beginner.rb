abf85b90e69ffaf7bcc1f20f2d9c0a39d47a34c7
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-01 13:00:10 -0500

    solution for level 4 beginner

--- level-3-beginner.rb
+++ level-3-beginner.rb
@@ -1,12 +1,27 @@
 #solution:
+
 class Player
+    #sets minimum allowable health as constant
+    MIN_HEALTH = 15
+
   def play_turn(warrior)
-    if warrior.feel.empty? && warrior.health < 20
-      warrior.rest!
-    elsif warrior.feel.empty? && warrior.health === 20
-      warrior.walk!
+    #the warrior checks if anything is ahead
+    if warrior.feel.empty?
+      #checks if warrior is not taking damage
+      if warrior.health < MIN_HEALTH && (warrior.health >= @previous_health)
+        #if the warrior is not taking damange it should rest
+        warrior.rest!
+      else
+        #otherwise it should walk
+        warrior.walk!
+      end
     else
+      #but if something is ahead it should attack
       warrior.attack!
     end
+
+    #warrior stores its end-of-turn health in an instance variable
+    @previous_health = warrior.health
+
   end
 end
